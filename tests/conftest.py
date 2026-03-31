6570f265852b53b393218fb420be35dc7ad343f2
TPS <tps@Gurdeeps-MacBook-Pro.local>
2026-03-31 22:30:04 +0530

    feat: add stops_client test fixture with host header

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,3 +28,25 @@ def db(app):
         for table in reversed(_db.metadata.sorted_tables):
             _db.session.execute(table.delete())
         _db.session.commit()
+
+
+@pytest.fixture(scope='function')
+def stops_client(app):
+    """Test client that sends Host header for stops.truckerpro.net domain."""
+    app.config['STOPS_DOMAIN'] = 'stops.localhost'
+    c = app.test_client()
+
+    class StopsClient:
+        """Wrapper that adds Host header to all requests."""
+        def __init__(self, client):
+            self._client = client
+
+        def get(self, *args, **kwargs):
+            kwargs.setdefault('headers', {})['Host'] = 'stops.localhost'
+            return self._client.get(*args, **kwargs)
+
+        def post(self, *args, **kwargs):
+            kwargs.setdefault('headers', {})['Host'] = 'stops.localhost'
+            return self._client.post(*args, **kwargs)
+
+    return StopsClient(c)
